d5b239cae512b1c21d2c33eafa4de3a080aa7748
tlqbw7 <tlqbw7@gmail.com>
2020-09-29 18:22:59 +0300

    Version 2

--- cp_2/Huziienko_fb-84_Liashko_fb-84_cp2/code.py
+++ cp_2/Huziienko_fb-84_Liashko_fb-84_cp2/code.py
@@ -1,7 +1,9 @@
 import codecs
 import re
-from itertools import cycle
+from collections import Counter
+
 alphabet=['а','б','в','г','д','е','ж','з','и','й','к','л','м','н','о','п','р','с','т','у','ф','х','ц','ч','ш','щ','ъ','ы','ь','э','ю','я']
+alp='абвгдежзийклмнопрстуфхцчшщъыьэюя'
 
 def Open_file(file):
  file_o=codecs.open(file,"r","utf-8")
@@ -11,9 +13,8 @@ def Open_file(file):
  #.replace('ъ','ь')
  regular=re.compile('[^а-яА-Я]')
  text=regular.sub('',text)
- print(text)
+ #print(text)
  return text;
-
 
 def Keys(num):
     if num in range(2,6):
@@ -28,9 +29,6 @@ def Keys(num):
     else:
         key='абстракционизм'
     return key;
-
-
-alp='абвгдежзийклмнопрстуфхцчшщъыьэюя'
 
 def Encr(text,alp,key):
     values=list()
@@ -59,25 +57,82 @@ def BigKey(key,text):
             Big_key+=key[i%len(key)] 
     #print(Big_key)
     return Big_key;
-def Menu(num,T):
+def Print_encr(num,T):
     print()
-    print('KEY: ', Keys(num))
+    print('KEY:', Keys(num))
+    print('r =',num)
     Big_key=BigKey(Keys(num),T)
     print("ENCRYPTED TEXT")
-    print(Encr(T,alp,Big_key))
+    encr_text=Encr(T,alp,Big_key)
+    print(encr_text)
+    
+def Enc_value(num,T):
+    Big_key=BigKey(Keys(num),T)
+    encr_text=Encr(T,alp,Big_key)
+    return encr_text
+    
+#COUNT
+def Letter_counter(text):
+    count=Counter(text)
+    return count;
 
+def Index(diction,n):
+    i=0
+    for key in diction:
+        i+=diction[key]*(diction[key]-1)
+    i=i*(1/(n*(n-1)))
+    return i
+
+def Index_check(diction,length):
+    index=0
+    for key in diction:
+        diction[key]=diction[key]/length
+        index+=diction[key]*diction[key]
+    return index
+def Print_index(text,n,num):
+    dic=Letter_counter(text)
+    index=Index(dic,len(text))
+    print('INDEX FOR TEXT')
+    if n==2:
+        print('With key length ',num)
+    print('I(Y) = ',index)
+    if n==1:
+        M=Open_file("text.txt")
+        dic_M=Letter_counter(M)
+        index_ch=Index_check(dic_M,len(M))
+        print('MI(Y) = ',index_ch)
+        
+    print()
+    
+    
+
+#main
+print("TASK 1")
+print()
+print("TEXT")
+T=Open_file("1.txt")
+print(T)
+print()
+for i in range(2,7,1):
+    if i==6:
+        Print_encr(14,T)
+    else:
+        Print_encr(i,T)
+    
+print("TASK 2")
+print()
+for i in range(1,7):
+    if i==1 or i==6:
+        if i==1:
+            Print_index(T,1,0)
+        else:
+            Print_index(Enc_value(14,T),2,14)
+    else:
+        Print_index(Enc_value(i,T),2,i)
+        
     
 
 
-
-#main
-print("ENCRYPTED TEXT")
-T=Open_file("1.txt")
-Menu(2,T)
-Menu(3,T)
-Menu(4,T)
-Menu(5,T)
-Menu(14,T)
 
 
 
